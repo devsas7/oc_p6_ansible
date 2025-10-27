2b956238a0e48766d94ee60777bee6a0cb8d9963
Sasmita Patel <developmentsasmita@gmail.com>
2025-10-27 07:48:49 +0100

    Initial commit: add ansible folder, Dockerfile, and main.tf

--- exo-2/docker/main.tf
+++ exo-2/docker/main.tf
@@ -1,3 +1,7 @@
+#############################################
+# Exercice 2 - Option Docker 
+#############################################
+
 terraform {
   required_providers {
     docker = {
@@ -9,48 +13,28 @@ terraform {
 
 provider "docker" {}
 
-
-resource "docker_image" "qemu" {
-  name = "qemux/qemu-docker:5.16"
+# Build custom image from Dockerfile
+resource "docker_image" "custom_ubuntu" {
+  name = "ubuntu-ssh:ansible"
+  build {
+    context = "${path.module}"
+    dockerfile = "${path.module}/Dockerfile"
+  }
 }
 
-resource "docker_volume" "qemu_storage" {
-  name = "openclassrooms-p6-exo2-qemu-storage"
-}
-
+# Start container with SSH
 resource "docker_container" "my_vm" {
-  image = docker_image.qemu.image_id
   name  = "openclassrooms-p6-exo2"
-  capabilities {
-    add = ["NET_ADMIN"]
-  }
+  image = docker_image.custom_ubuntu.image_id
 
-  env = [
-    "BOOT=https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.vmdk",
-    "ARGUMENTS=-drive if=virtio,format=raw,file=/share/cloud-init/cloud-init.img",
-  ]
   ports {
-    external = 8006
-    internal = 8006
-  }
-  ports {
-    external = 80
-    internal = 80
-  }
-  ports {
-    external = 2222
     internal = 22
-  }
-  devices {
-    host_path = "/dev/kvm"
+    external = 2222
   }
 
-  volumes {
-    host_path      = "${path.cwd}/share"
-    container_path = "/share"
-  }
-  volumes {
-    volume_name    = docker_volume.qemu_storage.id
-    container_path = "/storage"
-  }
+  # Keep container running with SSHD
+  command = ["/usr/sbin/sshd", "-D"]
+
+  # Restart policy optional
+  restart = "no"
 }
